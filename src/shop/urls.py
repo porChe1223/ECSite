72cf88554a3737472121118d758afef563310b64
porChe1223 <houshinri@gmail.com>
2025-03-14 02:05:10 +0900

    add: Productの詳細ページを追加

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', views.shop, name='shop'),
+    path('<int:pk>', views.product_detail, name="product_detail"),
 ]
--- src/shop/views.py
+++ src/shop/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def shop(request):
+    """""""""""""""""""""""""""""""""""
+    Productクラスの全データをHTMLに送信
+    """""""""""""""""""""""""""""""""""
     products = Product.objects.all()
 
     return render(request, 'shop.html', {'products': products})
+
+
+def product_detail(request, pk):
+    """""""""""""""""""""""""""""""""
+    Productクラスの単体をHTMLに送信
+    """""""""""""""""""""""""""""""""
+    product = get_object_or_404(Product, pk=pk)
+    
+    return render(request, 'product_detail.html', {"product": product})
+
