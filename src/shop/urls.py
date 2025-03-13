dc59eaa07c94c3de7180d851fff6d80d89ee5b90
porChe1223 <houshinri@gmail.com>
2025-03-14 01:35:51 +0900

    add: DBのProductをHTMLに出力

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="shop"),
+    path('', views.shop, name='shop'),
 ]
--- src/shop/views.py
+++ src/shop/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Product
 
-def index(request):
-    return HttpResponse("Hello!!")
+
+def shop(request):
+    products = Product.objects.all()
+
+    return render(request, 'shop.html', {'products': products})
